a937062843ce48dc206e309e37e2519c81f0305d
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-01-17 14:52:07 -0300

    Add Style and theme config (#2)
    ## Motivation
    
    Precisamos aplicar uma estilização global para utilizar na app e também precisamos de uma configuração de tema
    
    ## Proposed solution
    
    Criarmos um arquivo para usar as cores, fonts e radius da aplicação. Também precisamos criar um componente `Style` para a estilização global, utilizando o styled-components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
+import { Style } from 'ui'
+
 export function App() {
-  return <h1>E-cash</h1>
+  return (
+    <>
+      <h1>E-cash application</h1>
+      <Style />
+    </>
+  )
 }
---
+++ src/config/theme.ts
@@ -0,0 +1,25 @@
+export const theme = {
+  font: {
+    family: 'Barlow',
+    sizes: {
+      disclaimer: '1.2rem',
+      small: '1.4rem',
+      paragraph: '1.6rem',
+      subtitle: '2.2rem',
+      title: '3.2rem',
+    },
+  },
+  colors: {
+    white: '#ffffff',
+    blue: {
+      300: '#a4b7eA',
+      500: '#3a67e1',
+    },
+    neutral: {
+      100: '#eef1f5',
+      500: '#a1a7ae',
+      600: '#4d4f51',
+      900: '#18171d',
+    },
+  },
+}
