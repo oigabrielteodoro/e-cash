90b3808b420f75bdee98d40b8ae3a24637ef1db4
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-01-17 14:45:40 -0300

    Add theme colors

---
+++ src/ui/Style/Style.ts
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from 'styled-components'
+
+import { theme } from 'config'
+
+export const Style = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    height: 100vh;
+    overflow: hidden;
+    scroll-behavior: smooth;
+    font-family: ${theme.font.family};
+    font-size: ${theme.font.sizes.paragraph};
+    background-color: ${theme.colors.white};
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+`
