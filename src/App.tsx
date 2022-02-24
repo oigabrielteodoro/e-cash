6c4024becc7c6be6d4b7680a514b81dfd10c02f7
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-02-24 00:16:59 -0300

    Add invalidate queries when sign out successfully (#13)
    ## Motivation
    
    Precisamos limpar o cache do react-query quando o usuário deslogar da aplicação.
    
    ## Proposed solution
    
    Utilizar o invalidateQueries de dentro do queryClient da react-query, para limpar todo o cache existente.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 
-import { QueryClientProvider, QueryClient, DefaultOptions } from 'react-query'
+import {
+  QueryClientProvider,
+  QueryClient,
+  DefaultOptions,
+  QueryCache,
+} from 'react-query'
 
 import { Style, ToastContainer } from 'ui'
 import { Router } from 'lib'
 import { queryConfigDefault } from 'client'
 
-const queryClient = new QueryClient({
+export const queryClient = new QueryClient({
   defaultOptions: queryConfigDefault as DefaultOptions,
+  queryCache: new QueryCache(),
 })
 
 export function App() {
