ae23a7f5d7052006b94d16b87b058907d2faea63
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-02-28 21:56:17 -0300

    test: add snapshots tests in steps component

--- src/ui/Steps/Steps.test.tsx
+++ src/ui/Steps/Steps.test.tsx
@@ -27,6 +27,12 @@ function MockedComponent() {
 }
 
 describe('Steps', () => {
+  it('should be able match snapshot', () => {
+    const { container } = render(<MockedComponent />)
+
+    expect(container).toMatchSnapshot()
+  })
+
   it('should be able render correctly', () => {
     render(<MockedComponent />)
 
